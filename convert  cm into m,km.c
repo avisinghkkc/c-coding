20531d581d38009eafca7485d85f2b05afef3ee0
avisinghkkc <125532962+avisinghkkc@users.noreply.github.com>
2023-02-16 11:00:19 +0530

    Add files via upload

---
+++ convert  cm into m,km.c
@@ -0,0 +1,24 @@
+
+
+#include<stdio.h>
+int main()
+{
+float cm, m,km;
+printf("enter the length in cm\n");
+scanf("%f",&cm);
+m=cm/100 ;
+km=cm/100000 ;
+printf("%f",m);
+printf("%f",km);
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
