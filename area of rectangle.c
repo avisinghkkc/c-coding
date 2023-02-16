9546d367a557be35d21a65de49b2cd77deeee6ca
avisinghkkc <125532962+avisinghkkc@users.noreply.github.com>
2023-02-16 10:59:23 +0530

    Add files via upload

---
+++ area of rectangle.c
@@ -0,0 +1,27 @@
+
+#incl
+
+#include<stdio.h>
+void main()
+{
+    int x,y,a;
+    printf("enter the value x\n");
+    scanf("%d",&x);
+        printf("enter the value y\n");
+    scanf("%d",&y);
+    a=x*y;
+    printf("%d",a);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
