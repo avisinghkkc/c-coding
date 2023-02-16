9f6b2f378d111e769c0bfef3c108983e82d6cc40
avisinghkkc <125532962+avisinghkkc@users.noreply.github.com>
2023-02-16 10:54:54 +0530

    Add files via upload

---
+++ Sum of two number.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int x,y, a;
+    printf("enter the value x\n");
+    scanf("%d",&x);
+    printf("enter the value y\n");
+    scanf("%d",&y);
+    a=x+y;
+    printf("a :%d",a);
+    
+    return 0;
+
+}
