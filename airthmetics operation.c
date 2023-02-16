8f1d351cfeb589186c25f529421d61d6835bd885
avisinghkkc <125532962+avisinghkkc@users.noreply.github.com>
2023-02-16 10:57:00 +0530

    Add files via upload

---
+++ airthmetics operation.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main()
+{ 
+    int x,y, a,s,m,d;
+    printf("enter the value x\n");
+    scanf("%d",&x);
+    printf("enter the value y\n");
+    scanf("%d",&y);
+    a=x+y;
+    printf("%d",a);
+
+    return 0;
+}
