2a8d4b1a3c2b1dcf0b7275bc4f927933a5689d74
avisinghkkc <125532962+avisinghkkc@users.noreply.github.com>
2023-02-16 11:01:50 +0530

    Add files via upload

---
+++ rectangle and find its perimeter.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+   int x,y,p;
+   printf("enter the value x\n");
+   scanf("%d",&x);
+   printf("enter the value y\n");
+   scanf("%d",&y);
+   p=2*(x+y);
+   printf("%d",p);
+   
+}
